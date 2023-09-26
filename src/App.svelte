638c4e3e788a6faae9dc8a47b307040e9919fd47
Lukas Weibel <lukasweibel@macbook-air-von-lukas.home>
2023-09-26 20:17:57 +0200

    Delete Bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -21,48 +21,19 @@
   });
 </script>
 
-<head>
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-  ></script>
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-    crossorigin="anonymous"
-  />
-</head>
-
 <div id="container">
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#/"
-          ><img src="cevilogo.png" alt="background image" height="60px" /></a
-        >
+  <nav class="navbar">
+    <div class="navbar-content">
+      <a href="#/"
+        ><img src="cevilogo.png" alt="background image" height="60px" /></a
+      >
+      <div class="nav-links">
         {#if $accessToken}
-          <a class="nav-item nav-link" href="#/anschlagform"
-            >Anschlag erstellen</a
-          >
-          <a class="nav-item nav-link" href="#/persons">Personen</a>
+          <a href="#/anschlagform">Anschlag erstellen</a>
+          <a href="#/persons">Personen</a>
         {/if}
         {#if !$accessToken}
           <a
-            class="nav-item nav-link"
             href="https://db.cevi.ch/oauth/authorize?response_type=code&client_id=70AEhO3TpqcvA-9shANbACuhuBR4JZ3eTypJ8HpzaxE&redirect_uri=https://anschlagskasten-web-fd337ce2917a.herokuapp.com&scope=with_roles"
             >Login</a
           >
@@ -81,17 +52,25 @@
     box-sizing: border-box;
   }
 
-  html,
-  body {
-    height: 100%;
-    margin: 0;
-  }
-
   .navbar {
-    flex: 0 0 auto; /* Let the navbar size itself based on content */
+    flex: 0 0 auto;
     min-height: 60px;
     border-bottom: black solid 2px;
     background-color: rgb(166, 40, 52);
+    display: flex;
+    justify-content: flex-start; /* Align content to the left */
+    align-items: center;
+  }
+
+  .navbar-content {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav-links {
+    display: flex;
+    align-items: center;
+    margin-left: 10px; /* Add a small left margin */
   }
 
   a {
@@ -99,6 +78,7 @@
     font-size: 36px;
     margin-left: 10px;
     margin-right: 10px;
+    text-decoration: none;
   }
 
   #content {
